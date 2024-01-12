d84c4a9e8338843c6b018267488b1144e0dd1770
Lovedeep <gurugobind.sh@gmail.com>
2024-01-12 23:42:36 +0530

    Stripe Integration 1

---
+++ src/components/UpgadeButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type UpgadeButtonProps = {
+    
+};
+
+const UpgadeButton:React.FC<UpgadeButtonProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default UpgadeButton;
