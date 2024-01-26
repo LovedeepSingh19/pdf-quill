6aab656488be5a28ab17c9c3821567ac4b33df4e
Lovedeep <gurugobind.sh@gmail.com>
2024-01-26 18:12:02 +0530

    .

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,14 +1,3 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: any
-): Promise<void | NextResponse> {
-  const endpoint = params.kindeAuth
-  const result = await handleAuth(request, endpoint)
-
-  // Handle the result and return a compatible type
-  // For example, assuming handleAuth returns a NextResponse:
-  return result;
-}
+export const GET = handleAuth();
